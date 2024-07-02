787bd242cea0c86b66a90a5baa6bff14b156c9e5
Ritesh9876 <riteshmangdare@gmail.com>
2024-07-02 21:38:07 +0530

    Protocol Parsing: multiple request from client

--- server.cpp
+++ server.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,12 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <iostream>
+
+
+const size_t k_max_msg = 4096;
+
+
 
 static void die(const char *msg)
 {
@@ -20,20 +27,98 @@ static void msg(const char *msg)
     fprintf(stderr, "%s\n", msg);
 }
 
-static void do_something(int connfd)
+static int32_t do_something(int connfd)
 {
     char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    int32_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    printf("reading data %d\n",n);
+    if(n==0) return n;
     if (n < 0)
     {
         msg("read() error");
-        return;
+        return n;
     }
     printf("client says: %s\n", rbuf);
 
     char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+    return write(connfd, wbuf, strlen(wbuf));
 }
+
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        //printf("read full %ld \n",rv);
+        buf[6]='\0';
+        printf("read full buf before %s \n",buf);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+        //printf("read full buf after %s \n \n",buf);
+
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    //std::cout<<"first length of proto "<<len<<std::endl;
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    //printf("Request ends here \n \n");
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+}
+
 
 int main()
 {
@@ -91,7 +176,7 @@ int main()
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234); // ntohs function takes a 16-bit number in TCP/IP network byte order (the AF_INET or AF_INET6 address family) and returns a 16-bit number in host byte order.
     // takes a 32-bit number in TCP/IP network byte order (the AF_INET or AF_INET6 address family) and returns a 32-bit number in host byte order.
-    addr.sin_addr.s_addr = ntohl(0); // wildcard address 0.0.0.0
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // wildcard address 0.0.0.0
     int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
     if (rv)
     {
@@ -110,6 +195,7 @@ int main()
 
     while (true)
     {
+        //printf("how many times \n");
         // accept
         struct sockaddr_in client_addr = {};
         socklen_t addrlen = sizeof(client_addr);
@@ -125,14 +211,19 @@ int main()
         */
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
 
-        int x= getsockname(fd,(struct sockaddr *)&client_addr,&addrlen);
-       printf("x  is %d",x);
         if (connfd < 0)
         {
             continue; // error
         }
-       
-        do_something(connfd);
+
+         while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
+
+
         close(connfd);
     }
 }
