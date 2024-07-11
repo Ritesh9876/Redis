2a84454524bb52dfa7cfd950e9d87fc2f2c0915d
Ritesh9876 <riteshmangdare@gmail.com>
2024-07-11 05:41:32 +0530

    get, set, del request for simple server

--- client.cpp
+++ client.cpp
@@ -8,7 +8,11 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <string>
+#include <vector>
+#include <iostream>
 
+using namespace std;
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -48,16 +52,29 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
 
 const size_t k_max_msg = 4096;
 
-// the `query` function was simply splited into `send_req` and `read_res`.
-static int32_t send_req(int fd, const char *text) {
-    uint32_t len = (uint32_t)strlen(text);
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+    uint32_t len = 4;
+    for (const std::string &s : cmd) {
+        len += 4 + s.size();
+    }
     if (len > k_max_msg) {
         return -1;
     }
 
     char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4);  // assume little endian
-    memcpy(&wbuf[4], text, len);
+    cout<<"length at start "<<len<<endl;
+    memcpy(&wbuf[0], &len, 4);  // assume little endian
+    uint32_t n = cmd.size();
+    for(auto it: cmd) cout<<it<<" ";
+    cout<<"cmd len is "<<n<<endl;
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+    for (const std::string &s : cmd) {
+        uint32_t p = (uint32_t)s.size();
+        memcpy(&wbuf[cur], &p, 4);
+        memcpy(&wbuf[cur + 4], s.data(), s.size());
+        cur += 4 + s.size();
+    }
     return write_all(fd, wbuf, 4 + len);
 }
 
@@ -89,13 +106,18 @@ static int32_t read_res(int fd) {
         return err;
     }
 
-    // do something
-    rbuf[4 + len] = '\0';
-    printf("server says: %s\n", &rbuf[4]);
+    // print the result
+    uint32_t rescode = 0;
+    if (len < 4) {
+        msg("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
     return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
         die("socket()");
@@ -104,26 +126,24 @@ int main() {
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(0);  // 127.0.0.1 inet_addr("127.0.0.1") INADDR_LOOPBACK
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
     int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv) {
         die("connect");
     }
 
-
-    // multiple pipelined requests
-    const char *query_list[3] = {"hello1", "hello2", "hello3"};
-    for (size_t i = 0; i < 3; ++i) {
-        int32_t err = send_req(fd, query_list[i]);
-        if (err) {
-            goto L_DONE;
-        }
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; ++i) {
+        cout<<argv[i]<<endl;
+        cmd.push_back(argv[i]);
     }
-    for (size_t i = 0; i < 3; ++i) {
-        int32_t err = read_res(fd);
-        if (err) {
-            goto L_DONE;
-        }
+    int32_t err = send_req(fd, cmd);
+    if (err) {
+        goto L_DONE;
+    }
+    err = read_res(fd);
+    if (err) {
+        goto L_DONE;
     }
 
 L_DONE:
--- server.cpp
+++ server.cpp
@@ -12,6 +12,10 @@
 #include <netinet/ip.h>
 #include <vector>
 #include<iostream>
+#include<map>
+#include <sys/epoll.h>
+
+using namespace std;
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -111,6 +115,116 @@ static int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd) {
 static void state_req(Conn *conn);
 static void state_res(Conn *conn);
 
+const size_t k_max_args = 1024;
+
+
+static int32_t parse_req(
+    const uint8_t *data, size_t len, std::vector<std::string> &out)
+{
+    if (len < 4) {
+        return -1;
+    }
+    uint32_t n = 0;
+    memcpy(&n, &data[0], 4);
+    if (n > k_max_args) {
+        return -1;
+    }
+
+    size_t pos = 4;
+    while (n--) {
+        if (pos + 4 > len) {
+            return -1;
+        }
+        uint32_t sz = 0;
+        memcpy(&sz, &data[pos], 4);
+        if (pos + 4 + sz > len) {
+            return -1;
+        }
+        out.push_back(std::string((char *)&data[pos + 4], sz));
+        pos += 4 + sz;
+    }
+    cout<<"command read in server: ";
+    for(auto it: out) cout<<it<<" ";
+    cout<<endl;
+
+    if (pos != len) {
+        return -1;  // trailing garbage
+    }
+    return 0;
+}
+
+enum {
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2,
+};
+
+// The data structure for the key space. This is just a placeholder
+// until we implement a hashtable in the next chapter.
+static std::map<std::string, std::string> g_map;
+
+static uint32_t do_get(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    if (!g_map.count(cmd[1])) {
+        return RES_NX;
+    }
+    std::string &val = g_map[cmd[1]];
+    assert(val.size() <= k_max_msg);
+    memcpy(res, val.data(), val.size());
+    *reslen = (uint32_t)val.size();
+    return RES_OK;
+}
+
+static uint32_t do_set(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    (void)res;
+    (void)reslen;
+    g_map[cmd[1]] = cmd[2];
+    return RES_OK;
+}
+
+static uint32_t do_del(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    (void)res;
+    (void)reslen;
+    g_map.erase(cmd[1]);
+    return RES_OK;
+}
+
+static bool cmd_is(const std::string &word, const char *cmd) {
+    return 0 == strcasecmp(word.c_str(), cmd);
+}
+
+
+static int32_t do_request(
+    const uint8_t *req, uint32_t reqlen,
+    uint32_t *rescode, uint8_t *res, uint32_t *reslen)
+{
+    std::vector<std::string> cmd;
+    if (0 != parse_req(req, reqlen, cmd)) {
+        msg("bad req");
+        return -1;
+    }
+    if (cmd.size() == 2 && cmd_is(cmd[0], "get")) {
+        *rescode = do_get(cmd, res, reslen);
+    } else if (cmd.size() == 3 && cmd_is(cmd[0], "set")) {
+        *rescode = do_set(cmd, res, reslen);
+    } else if (cmd.size() == 2 && cmd_is(cmd[0], "del")) {
+        *rescode = do_del(cmd, res, reslen);
+    } else {
+        // cmd is not recognized
+        *rescode = RES_ERR;
+        const char *msg = "Unknown cmd";
+        strcpy((char *)res, msg);
+        *reslen = strlen(msg);
+        return 0;
+    }
+    return 0;
+}
+
 static bool try_one_request(Conn *conn) {
     // try to parse a request from the buffer
     if (conn->rbuf_size < 4) {
@@ -129,13 +243,22 @@ static bool try_one_request(Conn *conn) {
         return false;
     }
 
-    // got one request, do something with it
-    printf("client says: %.*s\n", len, &conn->rbuf[4]);
+    uint32_t rescode = 0;
+    uint32_t wlen = 0;
+    int32_t err = do_request(
+        &conn->rbuf[4], len,
+        &rescode, &conn->wbuf[4 + 4], &wlen
+    );
+    if (err) {
+        conn->state = STATE_END;
+        return false;
+    }
 
-    // generating echoing response
-    memcpy(&conn->wbuf[0], &len, 4);
-    memcpy(&conn->wbuf[4], &conn->rbuf[4], len);
-    conn->wbuf_size = 4 + len;
+
+    wlen += 4;
+    memcpy(&conn->wbuf[0], &wlen, 4);
+    memcpy(&conn->wbuf[4], &rescode, 4);
+    conn->wbuf_size = 4 + wlen;
 
     // remove the request from the buffer.
     // note: frequent memmove is inefficient.
@@ -158,12 +281,12 @@ static bool try_fill_buffer(Conn *conn) {
     // try to fill the buffer
     assert(conn->rbuf_size < sizeof(conn->rbuf));
     ssize_t rv = 0;
-    std::cout<<"new try read "<<std::endl;
+  //  std::cout<<"new try read "<<std::endl;
     do {
         size_t cap = sizeof(conn->rbuf) - conn->rbuf_size;
         rv = read(conn->fd, &conn->rbuf[conn->rbuf_size], cap);
         // rv -> 11 is EAGAIN which means It means "there is no data available right now, try again later".
-        std::cout<<cap<<" "<<rv<<" "<<errno<<std::endl;
+        //std::cout<<cap<<" "<<rv<<" "<<errno<<std::endl;
     } while (rv < 0 && errno == EINTR); // #define	EINTR -> 4	/* Interrupted system call */
 
     if (rv < 0 && errno == EAGAIN) {
@@ -371,7 +494,6 @@ int main()
         // process active connections
         for (size_t i = 1; i < poll_args.size(); ++i)
         {
-        std::cout<<poll_args[i].revents<<std::endl;
 
             if (poll_args[i].revents)
             {
@@ -389,6 +511,8 @@ int main()
         }
 
         // try to accept a new connection if the listening fd is active
+        // std::cout<<poll_args[0].revents<<std::endl;
+
         if (poll_args[0].revents)
         {
             (void)accept_new_conn(fd2conn, fd);
